4bf0d5abd549edfdac627ca1fdf9a40cbd04a3a4
bleedred23 <cool.redstars@gmail.com>
2024-10-12 14:45:08 +0500

    transactions / add findById method

--- src/repository/pg_transaction.go
+++ src/repository/pg_transaction.go
@@ -35,15 +35,15 @@ func (r *pgTransactionRepository) FindAll(ctx context.Context) ([]entity.Transac
 }
 
 func (r *pgTransactionRepository) FindById(ctx context.Context, id int) (entity.Transaction, error) {
-	transaction := entity.Transaction{}
+	transaction := &entity.Transaction{}
 
 	query := "SELECT * FROM transactions WHERE id = $1"
 
-	if err := r.DB.Select(&transaction, query, id); err != nil {
-		return transaction, err
+	if err := r.DB.GetContext(ctx, transaction, query, id); err != nil {
+		return *transaction, err
 	}
 
-	return transaction, nil
+	return *transaction, nil
 }
 
 func (r *pgTransactionRepository) Add(ctx context.Context, u entity.Transaction) error {
